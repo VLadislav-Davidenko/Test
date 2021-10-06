266cd5c104c7c79387a77f5285c773b49547e664
Visl <5vladik068@gmail.com>
2021-10-06 22:20:44 +0300

    My second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            System.out.println("Hello");
+            System.out.println("Hello World");
         }
     }
 }
